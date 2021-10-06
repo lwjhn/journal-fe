444c79cb4fed8f7c8dd650404a930cdbce22c15d
zhuanglw <lwjhn0118@163.com>
2021-10-06 22:24:28 +0800

    -'

--- src/views/statistic/stat-dept/lib/stat.js
+++ src/views/statistic/stat-dept/lib/stat.js
@@ -53,9 +53,9 @@ const modeConfig = {
             let year = this.where[0][0]
             year = year.value && year.value !== _ALL_CATEGORY_ ? (year.value + '年') : ''
             let company = this.where[0][2]
-            company = company.value && company.value !== _ALL_CATEGORY_ ? company.value : '本处室订'
+            company = company.value && company.value !== _ALL_CATEGORY_ ? company.value : '本处室'
             const title = `<tr><td class="stat-result-title none-border-has-bottom" style="padding-bottom: 5px!important" colspan="${this.result.columns.length + 1}">
-                            ${year}${company}阅统计</td></tr>`
+                            ${year}${company}订阅统计</td></tr>`
             const colTitle = extension.call(this)
             const len = this.result.data.length
             Object.assign(this.result, {
