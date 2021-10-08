5274f51416b839f522c6471ac620a545ee4cbdd3
曹飞霞 <1553155077@qq.com>
2021-10-08 17:18:40 +0800

    fix:修改工单16024

--- src/views/DbInterface/config/SubscriptionNonJoin.js
+++ src/views/DbInterface/config/SubscriptionNonJoin.js
@@ -9,7 +9,7 @@ import {
 } from './base-config'
 import service from '../../../service'
 import form from '../../form'
-import {callViewApproval} from "../../form/subscription-form/approval";
+import { callViewApproval } from "../../form/subscription-form/approval";
 
 export const page = form.SubscriptionForm
 export const model = service.models.subscription
@@ -17,7 +17,7 @@ export const tableAlias = 'subscription.'
 export const orderAlias = 'order.'
 export const paperAlias = 'paper.'
 
-export function beforeRequest(query, category, isCategory, forceJoin) {
+export function beforeRequest (query, category, isCategory, forceJoin) {
     query.tableAlias = tableAlias.replace(/\./, '')
     query.model = model.model
     if (forceJoin || !isCategory) {
@@ -41,7 +41,7 @@ export function beforeRequest(query, category, isCategory, forceJoin) {
 
     if (!isCategory) {
         let val = [], exp = this.columns.filter(item => !/sum|group|count|avg|wm_concat/i.test(item.expression)).map(item => {
-            let {expression, value} = item.group ? item.group : item
+            let { expression, value } = item.group ? item.group : item
             if (/\?/.test(expression))
                 val.splice(val.length, 0, value)
             return expression
@@ -56,7 +56,7 @@ export function beforeRequest(query, category, isCategory, forceJoin) {
     return query
 }
 
-function buttons() {
+function buttons () {
     let view = service.url.getUrlHashParam('view'),    //window.location.hash.match(/(^|&|\?|\#)view=([^&]*)(&|$)/i),
         mode = parseInt(service.url.getUrlHashParam('type'))
 
@@ -65,29 +65,29 @@ function buttons() {
             label: '通过审核',
             title: '通过审核',
             type: 'primary',
-            handle() {
+            handle () {
                 callViewApproval.call(this, mode, false)
             }
         }, {
             label: '不通过审核',
             title: '不通过审核',
             type: 'primary',
-            handle() {
+            handle () {
                 callViewApproval.call(this, mode, true)
             }
         }] : [{
             label: '取消审核',
             title: '取消审核',
             type: 'primary',
-            handle() {
+            handle () {
                 callViewApproval.call(this, mode, true)
             }
         }]
     ))
 }
 
-function category() {
-    return !/subscription/i.test(service.url.getUrlHashParam('view')) || /^0$/i.test(service.url.getUrlHashParam('type'))  ? [] : [
+function category () {
+    return !/subscription/i.test(service.url.getUrlHashParam('view')) || /^0$/i.test(service.url.getUrlHashParam('type')) ? [] : [
         {
             expression: 'subscribeYear',
             label: '年度',
@@ -107,7 +107,7 @@ function category() {
             label: '费用类型',
             width: '100px',
             desc: true,
-            criteria(item) {
+            criteria (item) {
                 return {
                     expression: `${item.group.expression} = ${item.value === '公费' ? 'TRUE' : 'FALSE'}`
                 }
@@ -119,8 +119,8 @@ function category() {
     ]
 }
 
-function replaceComma(value){
-    return typeof value ==='string' ? value.replace(/,/g, '、') : ''
+function replaceComma (value) {
+    return typeof value === 'string' ? value.replace(/,/g, '、') : ''
 }
 
 
@@ -137,30 +137,30 @@ export default function () {
                 hidden: true
             },
             ...(mode === 0 ? [{
+                expression: tableAlias + 'subscribeYear',
+                label: '订阅年度',
+                width: '120',
+                sortable: 'DESC',
+            }] : [
+                {
                     expression: tableAlias + 'subscribeYear',
-                    label: '订阅年度',
-                    width: '120',
+                    alias: service.camelToUpperUnderscore('subscribeYear'),
+                    hidden: true
+                }, {
+                    expression: tableAlias + 'subscribeOrgNo',
+                    alias: service.camelToUpperUnderscore('subscribeOrgNo'),
+                    hidden: true
+                }, {
+                    expression: tableAlias + 'subscribeTime',
+                    alias: service.camelToUpperUnderscore('subscribeTime'),
+                    label: '订阅时间',
+                    width: '180',
                     sortable: 'DESC',
-                }] : [
-                    {
-                        expression: tableAlias + 'subscribeYear',
-                        alias: service.camelToUpperUnderscore('subscribeYear'),
-                        hidden: true
-                    }, {
-                        expression: tableAlias + 'subscribeOrgNo',
-                        alias: service.camelToUpperUnderscore('subscribeOrgNo'),
-                        hidden: true
-                    }, {
-                        expression: tableAlias + 'subscribeTime',
-                        alias: service.camelToUpperUnderscore('subscribeTime'),
-                        label: '订阅时间',
-                        width: '180',
-                        sortable: 'DESC',
-                        format(option, item) {
-                            return service.formatStringDate(item.subscribeTime, 'yyyy-MM-dd hh:mm')
-                        }
+                    format (option, item) {
+                        return service.formatStringDate(item.subscribeTime, 'yyyy-MM-dd hh:mm')
                     }
-                ]
+                }
+            ]
             ), {
                 expression: 'subscribeOrg',
                 alias: service.camelToUpperUnderscore('subscribeOrg'),
@@ -171,7 +171,7 @@ export default function () {
                 alias: 'publication',
                 label: '报刊名称',
                 minWidth: '120',
-                format(option, item) {
+                format (option, item) {
                     return replaceComma(item.publication);
                 }
             }, /*{
@@ -208,7 +208,7 @@ export default function () {
                 alias: service.camelToUpperUnderscore('verifyStatus'),
                 label: '状态',
                 width: '100',
-                format(option, item) {
+                format (option, item) {
                     let status;
                     return item.hasOwnProperty(option.expression)
                         ? ((status = item[option.expression]) === 1 ? '待审核' : (status === 2 ? '已审核' : '草稿')) : '草稿'
@@ -219,39 +219,10 @@ export default function () {
         keyword: `${paperAlias}publication LIKE ? OR ${paperAlias}postalDisCode LIKE ? OR ${tableAlias}subscribeUser LIKE ? OR ${tableAlias}subscribeOrg LIKE ?`,
         search: searchOptions.call(this, [
             {
-                label: '订阅类型',
-                value: _ALL_CATEGORY_,
-                width: '300px',
-                criteria(item) {
-                    return item.value && item.value !== _ALL_CATEGORY_ ? {
-                        expression: `${tableAlias}govExpense=${item.value === '公费' ? 'TRUE' : 'FALSE'}`
-                    } : null
-                },
-                type: 'radio',
-                options: [_ALL_CATEGORY_OPTION_, {label: '自费'}, {label: '公费'}]
-            }, {
-                label: '报刊名称',
-                width: '500px',
-                criteria(item) {
-                    return item.value ? {
-                        expression: `${paperAlias}publication LIKE ?`,
-                        value: `%${item.value}%`
-                    } : null
-                }
-            }, {
-                label: '邮发代号',
-                width: '300px',
-                criteria(item) {
-                    return item.value ? {
-                        expression: `${paperAlias}postalDisCode LIKE ?`,
-                        value: `%${item.value}%`
-                    } : null
-                }
-            }, {
                 label: '订阅年份',
                 value: _ALL_CATEGORY_,
-                width: '300px',
-                criteria(item) {
+                width: '200px',
+                criteria (item) {
                     return item.value && item.value !== _ALL_CATEGORY_ ? {
                         expression: `${tableAlias}subscribeYear=?`,
                         value: item.value
@@ -265,32 +236,25 @@ export default function () {
                     //group: 'subscribeYear', //可选
                     desc: true,
                 }
-            }, {
-                label: '订阅日期',
-                width: '500px',
-                value: undefined,
-                criteria(item) {
-                    if(!item.value || item.value.length<1)
-                        return null
-                    const template=[{operator: '>=', format: 'yyyy-MM-dd 00:00:00'}, {operator: '<=', format: 'yyyy-M-d 23:59:59'}]
-                    let expression = [], value =[]
-                    item.value.forEach((dateTime, index)=>{
-                        if(!!(dateTime=service.string2Date(dateTime))){
-                            expression.push(`${tableAlias}subscribeTime ${template[index].operator} ?`)
-                            value.push(service.formatDate(dateTime, template[index].format))
-                        }
-                    })
-                    return {
-                        expression: expression.join(' AND '),
-                        value: value
-                    }
+            },
+            {
+                label: '订阅类型',
+                value: _ALL_CATEGORY_,
+                width: '210px',
+                criteria (item) {
+                    return item.value && item.value !== _ALL_CATEGORY_ ? {
+                        expression: `${tableAlias}govExpense=${item.value === '公费' ? 'TRUE' : 'FALSE'}`
+                    } : null
                 },
-                type: 'date',
-            }, {
+                type: 'radio',
+                options: [_ALL_CATEGORY_OPTION_, { label: '自费' }, { label: '公费' }]
+            },
+            {
                 label: '订阅处室',
                 value: _ALL_CATEGORY_,
                 width: '300px',
-                criteria(item) {
+                colSpan: 'calc(100% - 680px)',
+                criteria (item) {
                     return item.value && item.value !== _ALL_CATEGORY_ ? {
                         expression: `${tableAlias}subscribeOrg=?`,
                         value: item.value
@@ -302,11 +266,51 @@ export default function () {
                     expression: `${tableAlias}subscribeOrg`,
                     desc: true,
                 }
+            },
+            {
+                label: '报刊名称',
+                width: '200px',
+                criteria (item) {
+                    return item.value ? {
+                        expression: `${paperAlias}publication LIKE ?`,
+                        value: `%${item.value}%`
+                    } : null
+                }
+            }, {
+                label: '邮发代号',
+                width: '210px',
+                criteria (item) {
+                    return item.value ? {
+                        expression: `${paperAlias}postalDisCode LIKE ?`,
+                        value: `%${item.value}%`
+                    } : null
+                }
+            }, {
+                label: '订阅日期',
+                width: '300px',
+                value: undefined,
+                criteria (item) {
+                    if (!item.value || item.value.length < 1)
+                        return null
+                    const template = [{ operator: '>=', format: 'yyyy-MM-dd 00:00:00' }, { operator: '<=', format: 'yyyy-M-d 23:59:59' }]
+                    let expression = [], value = []
+                    item.value.forEach((dateTime, index) => {
+                        if (!!(dateTime = service.string2Date(dateTime))) {
+                            expression.push(`${tableAlias}subscribeTime ${template[index].operator} ?`)
+                            value.push(service.formatDate(dateTime, template[index].format))
+                        }
+                    })
+                    return {
+                        expression: expression.join(' AND '),
+                        value: value
+                    }
+                },
+                type: 'date',
             }
         ], beforeRequest),
         buttons: buttons.call(this),
         rowClick: rowClick(page),
-        beforeRequest(query, category, isCategory) {
+        beforeRequest (query, category, isCategory) {
             beforeRequest.call(this, query, category, isCategory)
             if (this.$attrs.type) {
                 service.sql(query, 'verifyStatus = ?', this.$attrs.type)
