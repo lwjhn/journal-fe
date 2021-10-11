05d1589f9b1dcc5c77041b719f25f3117b30d215
zhuanglw <lwjhn0118@163.com>
2021-10-11 14:10:32 +0800

    -

--- src/views/statistic/stat-dept/lib/stat.js
+++ src/views/statistic/stat-dept/lib/stat.js
@@ -72,7 +72,7 @@ const modeConfig = {
                         copies+=item.copies
                         prices+=item.prices
                     }
-                    return `<tr><td>总计</td><td>总计：${count}类</td><td>总计：${copies}份</td><td>总金额：${prices}</td></tr>`
+                    return `<tr><td>总计</td><td>种类：${count}类</td><td>总计：${copies}份</td><td>总金额：${prices}</td></tr>`
                 }
             })
         }
